44acbbef69701f19cb1c91f8ba4a1d35cdc76a2e
VivekanandaReddy2233 <120734128+VivekanandaReddy2233@users.noreply.github.com>
2024-04-24 08:01:36 +0530

    Add files via upload

---
+++ Leetcode javascript for 30daysChallage/Timeout Cancellation.js
@@ -0,0 +1,27 @@
+/**
+ * @param {Promise} promise1
+ * @param {Promise} promise2
+ * @return {Promise}
+ */
+var addTwoPromises = async function(promise1, promise2) {
+    let[val1,val2]=await Promise.all([promise1,promise2])
+    return val1+val2;
+};
+
+/**
+ * addTwoPromises(Promise.resolve(2), Promise.resolve(2))
+ *   .then(console.log); // 4
+ *//**
+ * @param {Promise} promise1
+ * @param {Promise} promise2
+ * @return {Promise}
+ */
+var addTwoPromises = async function(promise1, promise2) {
+    let[val1,val2]=await Promise.all([promise1,promise2])
+    return val1+val2;
+};
+
+/**
+ * addTwoPromises(Promise.resolve(2), Promise.resolve(2))
+ *   .then(console.log); // 4
+ */
